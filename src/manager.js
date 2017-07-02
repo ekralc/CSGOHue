3a2b6fcb8094a940ddc04199ff909188cb93be1d
James <jamesc2400@gmail.com>
2017-07-02 15:55:51 +0100

    Initial commit

---
+++ src/manager.js
@@ -0,0 +1,48 @@
+const EventEmitter = require('events')
+
+class GameManager extends EventEmitter {
+  constructor (server) {
+    super()
+
+    this.bombStatus = null
+    this.phase = null
+    this.lastRequestTime = 0
+    this.winningTeam = null
+    this.lastData = null
+    const self = this
+
+    server.on('gameData', function (data) {
+      data = JSON.parse(data)
+      //console.log(data)
+      //console.log('\n')
+      self.lastRequestTime = data.provider.timestamp
+
+
+      if (data !== self.lastData) {
+        if (data.round) {
+          if (data.round.phase !== self.phase) {
+            self.phase = data.round.phase
+            self.emit('phase', self.phase)
+          }
+
+          if (data.round.bomb !== self.bombStatus) {
+            self.bombStatus = data.round.bomb
+            self.emit('bomb', self.bombStatus)
+          }
+
+          if (data.round.win_team) {
+            if (data.round.win_team !== self.winningTeam) {
+              self.winningTeam = data.round.win_team
+              self.emit('win', self.winningTeam)
+            }
+          } else {
+            self.winningTeam = null
+          }
+        }
+      }
+      self.lastData = data
+    })
+  }
+}
+
+module.exports = GameManager
